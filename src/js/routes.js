acac189ac08a61a5fc4a092f087faa7d5a6f0208
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-05-19 17:44:58 +0200

    add a comp for kind of 404 page

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,11 +5,13 @@ import App from './components/app';
 import Home from './components/home';
 import List from './components/list';
 import Greet from './components/greeting';
+import NoComp from './components/no-page';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="list" component={List} />
     <Route path="greet" component={Greet} />
+    <Route path="/*" component={NoComp} />
   </Route>
 );
