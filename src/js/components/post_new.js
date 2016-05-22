cbb24d41bb1d06d21e6df515ca113820bc87f9ec
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-05-22 22:21:16 +0200

    add some validation to the redux-form

--- src/js/components/post_new.js
+++ src/js/components/post_new.js
@@ -30,28 +30,64 @@ class PostsNew extends Component {
     //with handleSubmit(this.props.createPost) we give the responsibility for submiting the form to redux-form
     //by passing the action creator createPost, this action creator is available from this.props
 
+    //console.log(this.props);
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
         <div>
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" className="form-control" {...title} />
+          <input type="text" name="title"
+            className={`form-control ${title.touched && title.invalid ? 'form-error is-visible' : ''}`}
+            {...title} />
+          <p className="help-text">
+          { /*touched is a property from redux form that returns true until the input is modified somehow
+            we use this to only show the error message once the user has interacted with the form
+            */
+          }
+            {title.touched ? title.error : ''}
+          </p>
         </div>
 
         <div>
           <label htmlFor="categories">Categories</label>
-          <input type="text" name="categories" className="form-control" {...categories} />
+          <input type="text" name="categories"
+            className={`form-control ${categories.touched && categories.invalid ? 'form-error is-visible' : ''}`}
+            {...categories} />
+          <p className="help-text">
+            {title.touched ? categories.error : ''}
+          </p>
         </div>
 
         <div>
           <label htmlFor="content">Content</label>
-          <textarea name="content" className="form-control" {...content} />
+          <textarea name="content"
+            className={`form-control ${content.touched && content.invalid ? 'form-error is-visible' : ''}`}
+            {...content} />
+          <p className="help-text">
+            {title.touched ? content.error : ''}
+          </p>
         </div>
 
         <button type="submit">Submit</button>
       </form>
     );
   }
+}
+
+//here we add validation to the form using redux-form
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+  if(!values.content) {
+    errors.content = 'Enter some content';
+  }
+   return errors;
 }
 
 //connect: 1st argment is mapStateToProps, 2nd is mapDispatchToProps
@@ -63,7 +99,8 @@ class PostsNew extends Component {
 // which fields it contains and what they are called and wire it to redux:
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate //we connect redux-form validate helper to our local function
 }, null, {createPost})(PostsNew);
 
 // when user types something in...record it on application state like so:
